781b1bc665220af193aaf88d3d140b0286458e0e
Jared Hughes <jared.t.hughes-1@ou.edu>
2017-04-24 15:45:04 -0500

    Added a simple test fixture for views. Added a test fixture and stubs for the log in view

---
+++ Zoo Game/Assets/Scripts/Views/Editor/LogInViewTests.cs
@@ -0,0 +1,112 @@
+﻿
+using UnityEngine.UI;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+namespace Zoo.UI.Test
+{
+    /// <summary>
+    /// Tests the behavior of the log in view
+    /// </summary>
+    public class LogInViewTests : ViewTestFixture<LogInView>
+    {
+        /// <summary>
+        /// Extended to initialize all UI component on the view
+        /// </summary>
+        public override void InitializeView()
+        {
+            base.InitializeView();
+
+            View.Email = MakeBehaviour<InputField>();
+            View.Password = MakeBehaviour<InputField>();
+        }
+
+        #region Test Cases
+
+        /// <summary>
+        /// Tests the behavior of the SetEmailText function
+        /// </summary>
+        [Test]
+        public void TestSetEmailText()
+        {
+            var previous = View.Email.text;
+            var expected = previous + "_Test";
+
+            View.SetEmailText(expected);
+
+            Assert.AreNotEqual(previous, View.Email.text);
+            Assert.AreEqual(expected, View.Email.text);
+        }
+
+        /// <summary>
+        /// Tests the behavior of the SetPasswordText function
+        /// </summary>
+        [Test]
+        public void TestSetPasswordText()
+        {
+            var previous = View.Password.text;
+            var expected = previous + "_Test";
+
+            View.SetPasswordText(expected);
+
+            Assert.AreNotEqual(previous, View.Password.text);
+            Assert.AreEqual(expected, View.Password.text);
+        }
+
+        /// <summary>
+        /// Tests that the OnSubmitClicked event is properly executed
+        /// </summary>
+        [Test]
+        public void TestOnSubmitClickedEvent()
+        {
+            bool success = false;
+
+            View.OnSubmitClicked += () => {
+                success = true;
+            };
+            View.Submit();
+
+            Assert.IsTrue(success);
+        }
+
+        /// <summary>
+        /// Tests the behavior of the OnEmailTextChanged event
+        /// </summary>
+        [Test]
+        public void TestOnEmailTextChangedEvent()
+        {
+            var previous = View.Email.text;
+            var expected = previous + "t";
+
+            var text = previous;
+
+            View.OnEmailTextChanged += (t) => {
+                Assert.AreNotEqual(previous, t);
+                Assert.AreEqual(expected, t);
+            };
+
+            View.Email.text = expected;
+        }
+
+        /// <summary>
+        /// Tests the behavior of the OnPasswordChangedEvent event
+        /// </summary>
+        [Test]
+        public void TestOnPasswordChangedEvent()
+        {
+            var previous = View.Password.text;
+            var expected = previous + "t";
+
+            var text = previous;
+
+            View.OnPasswordTextChanged += (t) => {
+                Assert.AreNotEqual(previous, t);
+                Assert.AreEqual(expected, t);
+            };
+
+            View.Email.text = expected;
+        }
+
+        #endregion
+    }
+}
---
+++ Zoo Game/Assets/Scripts/Views/Editor/ViewTextFixture.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using NUnit.Framework;
+
+namespace Zoo.UI.Test
+{
+    /// <summary>
+    /// Used to test a view component. This base class sets up all data necessary for test fixtures
+    /// to operate
+    /// </summary>
+    /// <typeparam name="T">The type of view to test</typeparam>
+    public class ViewTestFixture<T> where T : MonoBehaviour
+    {
+        /// <summary>
+        /// The parent object of the view
+        /// </summary>
+        protected GameObject ViewObject { get; private set; }
+
+        /// <summary>
+        /// The view class
+        /// </summary>
+        protected T View { get { return ViewObject.GetComponent<T>(); } }
+
+        #region Setup
+
+        /// <summary>
+        /// Initializes the test fixture
+        /// </summary>
+        [SetUp]
+        public virtual void InitializeView()
+        {
+            ViewObject = MakeBehaviour<T>().gameObject;
+        }
+
+        /// <summary>
+        /// Convenience function to manually create a monobehaviour
+        /// </summary>
+        /// <typeparam name="B">The type of monobehaviour to create</typeparam>
+        /// <returns>Working instance of B</returns>
+        protected B MakeBehaviour<B>() where B : MonoBehaviour
+        {
+            var obj = new GameObject(string.Format("Simulated {0} Instance", typeof(B).Name));
+            obj.AddComponent<B>();
+            return obj.GetComponent<B>();
+        }
+
+        #endregion
+    }
+}
