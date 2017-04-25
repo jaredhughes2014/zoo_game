8b7a2af709e483c4955a389462f9c0353479eb13
Jared <j.hughes2014@ou.edu>
2017-04-24 21:20:35 -0500

    Added test fixtures and stubs for the HUD view

---
+++ Zoo Game/Assets/Scripts/Views/HUD/HUDView.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Zoo.UI
+{
+    /// <summary>
+    /// Renders the HUD
+    /// </summary>
+    public class HUDView : MonoBehaviour
+    {
+        #region Events
+
+        /// <summary>
+        /// Event executed when the user gestures to change the movement mode
+        /// </summary>
+        public delegate void OnChangeMovementModeHandler();
+        public event OnChangeMovementModeHandler OnChangeMovementMode;
+
+        #endregion
+
+        #region Input Handlers
+
+        /// <summary>
+        /// Executed from the scene to change the movement mode
+        /// </summary>
+        public void ChangeMovementMode()
+        {
+
+        }
+
+        #endregion
+    }
+}
